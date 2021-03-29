e76b185d9a5179496bc69ee39dc6c914e85cf778
Shumine <jah2jah.sr@gmail.com>
2021-03-29 16:47:48 +0200

    Adding first styles

---
+++ src/script.js
@@ -0,0 +1,8 @@
+// Create the "Ajouter un livre" button
+
+const addBookButton = document.getElementById('btn-add');;
+addBookButton.id = "addBook";
+addBookButton.innerHTML = "Ajouter un livre";
+divMyBooks.appendChild(addBookButton);
+
+});
