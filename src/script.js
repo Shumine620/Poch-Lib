3247bafa9a552034d5a9bccf868477cccce7ba58
Shumine <jah2jah.sr@gmail.com>
2021-04-10 23:05:09 +0200

    Working on displayBook

--- src/script.js
+++ src/script.js
@@ -1,7 +1,8 @@
 //Au Chargement de la page - Init page
 //Create the "Ajouter un livre" button
-function init(){
 
+function init(){
+   
     const div_myBooks = document.createElement('div');
     div_myBooks.id = 'myBooks';
     myBooks.appendChild(div_myBooks);
@@ -16,8 +17,7 @@ function init(){
         div_myBooks.appendChild(addBookButton);
     
         $("hr").before(addBookButton);
-        addBookButton.addEventListener('click',researchFields);
-     
+        addBookButton.addEventListener('click',researchFields); 
 }
 
  //Title of the book & Author fields + Search and cancel buttons
@@ -31,12 +31,13 @@ function researchFields(){
     inputTitleField.setAttribute("type", "search");
     inputTitleField.setAttribute("value", "Titre du livre");
     div_researchFields.appendChild(inputTitleField);
-  
+ 
    const inputAuthorField = document.createElement("input");
    inputAuthorField.id="inputAuthorField";
    inputAuthorField.setAttribute("type", "search");
    inputAuthorField.setAttribute("value", "Auteur");
    div_researchFields.appendChild(inputAuthorField);
+   inputAuthorField.style.display = 'inline-block';
   
    const div_researchButtons = document.createElement('div');
    div_researchFields.appendChild(div_researchButtons);
@@ -46,100 +47,108 @@ function researchFields(){
     searchButton.className = 'button';
     searchButton.innerHTML = "Rechercher ";
     div_researchButtons.appendChild(searchButton);
+    searchButton.style.display = 'inline-block';
      
     const cancelButton = document.createElement("button");
      cancelButton.id = 'cancelButton';
     cancelButton.className = 'button';
     cancelButton.innerHTML = "Annuler ";
     div_researchButtons.appendChild(cancelButton);
-   
+   cancelButton.style.display = 'inline-block';
+
     $("hr").before(div_researchFields);
    
-    cancelButton.addEventListener('click',clearInputFields);
-    searchButton.addEventListener('click',searchBook);
-      }
+    cancelButton.addEventListener('click',clearInputFields,resultField);
+   searchButton.addEventListener('click',resultField);
+  searchButton.addEventListener('click', searchBook);
+         }
+
+function resultField(){
   
+const pochListDiv = document.getElementById("content");
+   const resultDiv = document.createElement('div');
+   resultDiv.id = "resultDiv";
+   document.body.appendChild(resultDiv);
+   pochListDiv.before(resultDiv);
+
+   const bookResultDiv = document.createElement('div');
+   bookResultDiv.id = "resultsContent";
+   resultDiv.appendChild(bookResultDiv);
+
+   const myResults = document.createElement('h3');
+   myResults.id = 'myResults';
+   myResults.className = 'h3';
+   myResults.innerHTML = "Résultats de la recherche";
+ pochListDiv.style.display = "none";
+   bookResultDiv.before(myResults);
+    // displayBook();
+}
   //Cancel button clear the input fields
-  function clearInputFields(){
-      document.getElementById('researchFields');
-            document.querySelector("#myBooks > div:nth-child(4)").remove();  
-           
-  }
-
-
- async function getUrl(url) {
-
+function clearInputFields(){
+   
+    document.getElementById('researchFields');
+    document.querySelector("#myBooks > div:nth-child(4)").remove(); 
+    document.getElementById('resultField');
+    document.querySelector("#resultDiv").remove();
+     document.querySelector("#noBookFound").remove();
+     document.querySelector("#myResults").remove();
+    document.getElementById("content").style.display="block";
+}
+async function requestGet(url) {
     const response = await fetch(url);
     if (response.ok) {
         return response.json();
-
     } else {
-        console.error(response.status);
+        console.error("Ereur : " + response.status); 
     }
 }
-  //PochListDiv to received the search results & favorit books
-  function createPochListDiv(){
 
-    const pochList = document.getElementById('content');
-    const div_myPochList = document.createElement('div');  
-    div_myPochList.id = 'pochListDiv';
-    pochList.appendChild(div_myPochList); 
-    }
-   
 function searchBook(){
-   
+
+    const apiRequest = new XMLHttpRequest();
+    const apiK= "AIzaSyCe3Dpkc52IYszEgfE9uOq5OShSCvY_jDY";
+
     var title = document.getElementById('inputTitleField').value;
     var author = document.getElementById('inputAuthorField').value;
-        
-       //Alert message if the author and title fields are empty
-      
-    if (title == ''){
-        createErrorInput('title', 'Merci de renseigner le champ.');
-        return;
-        }
-    if (author == ''){
-        createErrorInput('author', 'Merci de renseigner le champ.');
-        return;
-        } 
-//createPochListDiv();
+    
+    if(title == "" || author == ""){
+		let error = document.getElementById("error");
+		error.innerHTML = "Veuillez renseigner les champs correspondants')";
+		return;
+	}
 
-const  apiRequest = new XMLHttpRequest();
-apiRequest.onreadystatechange = function() {
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+    apiRequest.onreadystatechange = function() {
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
 
-        let response = JSON.parse(this.response);
+            let results = JSON.parse(this.response);
 
-        if (response.items.length == 0) {
-            alert("Aucun livre trouvé");
-        } else {
-            response.items.map(item => {
-                displayBook(item.volumeInfo, item.id);
-            })
+            if (results.items === undefined) {
+                const noBookFound = document.createElement("p");
+                noBookFound.id= "noBookFound";
+                noBookFound.setAttribute("class", "noBookFound");
+                noBookFound.innerHTML = "Aucun livre n’a été trouvé";
+                document.querySelector("#myResults").appendChild(noBookFound);
+                document.querySelector("#content").before(noBookFound);
+            } else {
+                response.items.map(item => {
+                    displayBook(item.volumeInfo, item.id);
+                })
+            }
         }
     }
-}
-apiRequest.open("GET", "https://www.googleapis.com/books/v1/volumes?q=:" + author + title);
+    apiRequest.open("GET", "https://www.googleapis.com/books/v1/volumes?q=:" +"inauthor:"+ author + title + apiK);
 apiRequest.send();
 }
 
-
-//Create results fields to display books
-
-function displayBook(results, display) {
-
+function displayBook(results, display){
     const divBookList = document.createElement("div");
     divBookList.setAttribute("class", "bookList");
+    document.querySelector("#myResults").appendChild(divBookList);
 
-    if (results.items === undefined) {
-        const noBookFound = document.createElement("p");
-        noBookFound.setAttribute("class", "noBookFound");
-        noBookFound.innerHTML = "Aucun livre n’a été trouvé";
-        divBookList.appendChild(noBookFound);
-    } else {
-
-        results.items.map(item => {
-            const resultsBookList = document.createElement("div");
-            resultsBookList.setAttribute("class", "book");
+    results.items.map(item => {
+    const resultsBookList = document.createElement("div");
+     resultsBookList.setAttribute("class", "book");
+    resultsBookList.appendChild(divBookList);
 
             switch (display) {
                 case "0":
@@ -150,12 +159,10 @@ function displayBook(results, display) {
                     bookmark = document.createElement("i");
                     bookmark.setAttribute("class", "fas fa-dumpster");
                     break;
-
                 default:
-                    console.log("error affichage icon");
+                    console.log("Erreur d'affichage");
                     break;
             }
-
             resultsBookList.appendChild(bookmark); 
 
            const bookId = document.createElement("p");
@@ -163,53 +170,46 @@ function displayBook(results, display) {
             bookId.innerHTML = "ID: " + item.id;
             resultsBookList.appendChild(bookId);
 
+            const bookTitle = document.createElement("p");
+            bookTitle.setAttribute("class", "titlebook");
+            bookTitle.innerHTML = "Titre: " + item.volumeInfo && item.volumeInfo.title ? item.volumeInfo.title : '';
+            resultsBookList.appendChild(bookTitle);
 
-            const titleField = document.createElement("p");
-            titleField.setAttribute("class", "titlebook");
-            titleField.innerHTML = "Titre: " + item.volumeInfo && item.volumeInfo.title ? item.volumeInfo.title : '';
-            resultsBookList.appendChild(titleField);
-
-            const authorField = document.createElement("p");
-            authorField.setAttribute("class", "authorbook");
-            authorField.innerHTML = "Auteur: " + item["volumeInfo"].author;
-            resultsBookList.appendChild(newAuthorField);
+            const bookAuthor = document.createElement("p");
+            bookAuthor.setAttribute("class", "authorbook");
+            bookAuthor.innerHTML = "Auteur: " + item["volumeInfo"].author;
+            resultsBookList.appendChild(bookAuthor);
 
             //Checking description
             const description = document.createElement("p");
             description.setAttribute("class", "bookDescription");
-            
-            if (item["volumeInfo"].description != undefined) {
-                description.innerHTML =  item["volumeInfo"].description.substr(0, 200);
-            } else {
-                description.innerHTML = "Information manquante";
-            }
+            description.setAttribute("maxlength", "200");       
+    if (response.volumeInfo.description == undefined) {
+        description.textContent = "Description: Information manquante";
+    } else {
+      description.textContent = "Description: " + response.volumeInfo.description.substr(0, 200) + "...";
+    }
+    resultsBookList.appendChild(description);
+    
 
-            resultsBookList.appendChild(description);
-
-            // Checking image
+    //Checking image
             if (item["volumeInfo"].imageLinks != undefined) {
                 const image = document.createElement("img");
                 image.setAttribute("class", "image");
                 image.setAttribute("src", item["volumeInfo"].imageLinks.smallThumbnail);
 
             } else {
-
                 const image = document.createElement("img");
                 image.setAttribute("class", "image");
                 image.setAttribute("src", "logo/unavailable.png");
-
             }
             const div_Image = document.createElement("div");
             div_Image.appendChild(image);
             resultsBookList.appendChild(div_Image);
 
             divBookList.appendChild(resultsBookList);
-        });
-    }
-    return divBookList; }
-
-//<i class="fas fa-book-reader"></i>
-
-
+    })
+}
+//<i class="fas fa-book-reader"></i>*/
 
  window.addEventListener('DOMContentLoaded', init);
